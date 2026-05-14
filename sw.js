a9e1c30a44d89ae732ec6f93ea1fbc76c7da1824
mirajkhat-pixel <mirajkhat@gmail.com>
2026-05-14 05:11:53 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener('message', (event) => {
     self.skipWaiting();
   }
 });
-const CACHE_NAME = 'mageri-v3';
+const CACHE_NAME = 'mageri-v4';
 const urlsToCache = [
 // Install
 self.addEventListener('install', event => {
